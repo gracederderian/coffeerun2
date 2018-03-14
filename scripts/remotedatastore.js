ded46a604e3dbb64aabd0f5438629291ada6aff0
gracederderian <grace.derderian@csu.fullerton.edu>
2018-03-13 17:47:42 -0700

    Update remotesatastore.js

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -12,29 +12,81 @@
   }
 
   RemoteDataStore.prototype.add = function(key, val) {
-    $.post(this.serverUrl, val, function(serverResponse) {
-      /* eslint-disable no-console*/
+    /*$.post(this.serverUrl, val, function(serverResponse) {
       console.log(serverResponse);
+    });*/
+
+    $.ajax({
+      url: this.serverUrl,
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(val),
+      success: function(serverResponse) {
+        console.log(serverResponse);
+      },
+      error: function(xhr) {
+        alert(xhr.responseText);
+      }
     });
+
   };
 
   RemoteDataStore.prototype.getAll = function(cb) {
-    $.get(this.serverUrl, function(serverResponse) {
+    /*$.get(this.serverUrl, function(serverResponse) {
       console.log(serverResponse);
       cb(serverResponse);
+    });*/
+
+    $.ajax({
+      url: this.serverUrl + "coffeeorders/",
+      type: "GET",
+      success: function(serverResponse) {
+        console.log(serverResponse);
+        cb(serverResponse);
+      },
+      error: function(xhr) {
+        alert(xhr.responseText);
+      }
     });
+
   };
 
   RemoteDataStore.prototype.get = function(key, cb) {
-    $.get(this.serverUrl + "/" + key, function(serverResponse) {
+    /*$.get(this.serverUrl + "/" + key, function(serverResponse) {
       console.log(serverResponse);
       cb(serverResponse);
+    });*/
+
+    $.ajax({
+      url: this.serverUrl + "/" + key,
+      type: "GET",
+      success: function(serverResponse) {
+        console.log(serverResponse);
+        cb(serverResponse);
+      },
+      error: function(xhr) {
+        alert(xhr.responseText);
+      }
     });
+
   };
 
   RemoteDataStore.prototype.remove = function(key) {
-    $.ajax(this.serverUrl + "/" + key, {
-      type: "DELETE"
+    $.ajax({
+      url: this.serverUrl + "?emailAddress=" + key,
+      type: "GET",
+      success: function(serverResponse) {
+        $.ajax({
+          url: "http://localhost:2403/coffeeorders" + "/" + serverResponse[0].id,
+          type: "DELETE",
+          error: function(xhr) {
+            alert(xhr.responseText);
+          }
+        });
+      },
+      error: function(xhr) {
+        alert(xhr.responseText);
+      }
     });
   };
 
